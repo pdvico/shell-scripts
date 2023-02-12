f11378bd3c3f8d28086f0253b48b8350cc0b36f7
Pablo <4821937+pdvico@users.noreply.github.com>
2023-02-12 12:06:20 +0100

    Create instal-prerender.sh

---
+++ instal-prerender.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#---------------------------------------------------------------------------------
+# Creating and installing prerender container on running docker bridge network
+#---------------------------------------------------------------------------------
+# git clone src prerender
+git clone https://github.com/pdvico/prerender-alpine.git
+# place on prerender-alpine folder
+cd prerender-alpine 
+# build image from docker file
+sudo docker build -t prerender .
+# run docker image
+sudo docker run --name prerender prerender
